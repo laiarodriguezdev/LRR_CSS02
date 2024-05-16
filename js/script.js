d93e4ea378c82eccd6f53e4c7d8c9232aa708eaa
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-05-16 20:32:45 +0200

    Preparant js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const songs = [
+    {
+        title: "While Your Lips Are Still Red",
+        artist: "Nightwish",
+        duration: "4:21",
+        image: "path/to/image1.jpg",
+        audio: "./music/Nightwish.mp3"
+    },
+    {
+        title: "End of me",
+        artist: "Apocalyptica",
+        duration: "3:29",
+        image: "path/to/image2.jpg",
+        audio: "./music/Apocalyptica.mp3"
+    },
+    {
+        title: "Down With Sickness",
+        artist: "Violet Orlandi",
+        duration: "2:55",
+        image: "path/to/image3.jpg",
+        audio: "./music/DownWithSickness.mp3"
+    }
+];
+
+let titleSong = document.getElementById("title");
+let artistSong = document.getElementById("artist");
+let timeSong = document.getElementById("time");
